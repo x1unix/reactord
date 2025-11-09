e9080b8008957ebd58565142531f59f2210bb1f4
x1unix <9203548+x1unix@users.noreply.github.com>
2025-11-09 00:32:57 -0500

    feat: add basic pw listener

--- src/main.rs
+++ src/main.rs
@@ -2,47 +2,87 @@ use anyhow::{Context, Result};
 use pipewire as pw;
 use tokio::sync::mpsc;
 
-use pw::thread_loop::ThreadLoopRc;
+use pw::{context::ContextRc, core::CoreRc, registry::RegistryRc, thread_loop::ThreadLoopRc};
 
 fn new_thread_loop() -> Result<ThreadLoopRc, pw::Error> {
     unsafe { ThreadLoopRc::new(None, None) }
 }
 
-struct PWContext<'a> {
+struct PWContext {
     thread_loop: ThreadLoopRc,
-    context: pw::context::ContextBox<'a>,
-    core: pw::core::CoreBox<'a>,
-    registry: pw::registry::RegistryBox<'a>,
+    context: ContextRc,
+    core: CoreRc,
+    registry: RegistryRc,
 }
 
-impl<'a> PWContext<'a> {
-    fn new() -> Result<PWContext<'a>> {
-        pw::init();
+impl PWContext {
+    fn new() -> Result<PWContext> {
         let tloop = new_thread_loop().context("can't create thread loop")?;
-        let ctx = pw::context::ContextBox::new(&tloop.loop_(), None)
-            .context("can't create pw context")?;
-        let core = ctx.connect(None).context("can't connect to pw context")?;
-        let registry = core.get_registry().context("can't get pw registry")?;
+        let ctx = ContextRc::new(&tloop, None).context("can't create pw context")?;
+        let core = ctx
+            .connect_rc(None)
+            .context("can't connect to pw context")?;
+        let registry = core.get_registry_rc().context("can't get pw registry")?;
 
-        Ok(PWContext{
-            thread_loop = tloop,
-            context = ctx,
-            core = core,
-            registry = registry
+        Ok(PWContext {
+            thread_loop: tloop,
+            context: ctx,
+            core,
+            registry,
         })
     }
 }
 
-fn start_pw_thread(sender: mpsc::UnboundedSender<()>) -> Result<()> {
-    std::thread::spawn(move || {
-        println!("pw: starting...");
+fn start_pw_thread(
+    cancel_token: std::sync::mpsc::Receiver<()>,
+    _sender: Option<mpsc::UnboundedSender<()>>,
+) -> Result<std::thread::JoinHandle<()>> {
+    let h = std::thread::spawn(move || {
+        println!("pw: initializing...");
         pw::init();
-        println!("pw: started");
+        println!("pw: initialized");
+
+        let pwctx = match PWContext::new() {
+            Ok(r) => r,
+            Err(err) => {
+                eprintln!("failed to build pipewire consumer: {err}");
+                return;
+            }
+        };
+
+        println!("pw: registering listener...");
+        let _listener = pwctx
+            .registry
+            .add_listener_local()
+            .global(|global| {
+                println!("pw: new global - {global:?}");
+            })
+            .register();
+
+        println!("pw: starting thread loop...");
+        pwctx.thread_loop.start();
+
+        // Suspend thread until cancellation signal is sent.
+        // PW's ThreadLoop already manages its own thread under the hood.
+        cancel_token.recv().ok();
+
+        println!("pw: shutting down...");
+        pwctx.thread_loop.stop();
     });
 
-    Ok(())
+    Ok(h)
 }
 
 fn main() {
-    println!("Hello, world!");
+    // TODO: use SIGINT for cancellation signal
+    let (_stop_tx, stop_rx) = std::sync::mpsc::channel::<()>();
+    let h = match start_pw_thread(stop_rx, None) {
+        Ok(h) => h,
+        Err(err) => {
+            eprintln!("failed to start pipewire listener: {err}");
+            return;
+        }
+    };
+
+    h.join().unwrap();
 }
