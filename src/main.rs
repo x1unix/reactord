d82cf5310806881cfe0cdb7f4f7e9418c9443fd9
x1unix <9203548+x1unix@users.noreply.github.com>
2025-11-19 21:32:07 -0500

    feat: use tracing logger

--- src/main.rs
+++ src/main.rs
@@ -2,8 +2,10 @@ mod pwloop;
 mod state;
 mod utils;
 
-use anyhow::Result;
+use anyhow::{Context, Result};
 use state::{ActionType, State, VolumeInfo};
+use tracing::{debug, error, info, info_span, warn};
+use tracing_subscriber::{EnvFilter, layer::SubscriberExt, util::SubscriberInitExt};
 
 fn new_cancel_token() -> Result<std::sync::mpsc::Receiver<()>> {
     let (stop_tx, stop_rx) = std::sync::mpsc::channel::<()>();
@@ -22,6 +24,7 @@ fn format_volume(vol: &VolumeInfo) -> String {
     };
 
     // Bad but works for debug
+    // NOTE: for most real devices, volume will be in channel_volumes (as they're stereo).
     let level = match vol.volume {
         // FIXME: why volume here is always empty?
         Some(v) => format!("{}%", v * 100.0),
@@ -36,55 +39,64 @@ fn format_volume(vol: &VolumeInfo) -> String {
     format!("{level} {status}")
 }
 
-fn main() {
-    let stop_rx = new_cancel_token().expect("failed to init shutdown handler");
-    let h = match pwloop::start_pw_thread(stop_rx) {
-        Ok(h) => h,
-        Err(err) => {
-            eprintln!("failed to start pipewire listener: {err}");
-            return;
-        }
-    };
+fn init_logger() {
+    let env_filter = EnvFilter::try_from_default_env()
+        .or_else(|_| EnvFilter::try_new(format!("{}=warn", env!("CARGO_PKG_NAME"))))
+        .expect("failed to get log level from environment");
 
+    tracing_subscriber::registry()
+        .with(env_filter)
+        .with(tracing_subscriber::fmt::layer().with_target(true))
+        .init();
+}
+
+fn main() {
+    init_logger();
+    if let Err(err) = run() {
+        eprintln!("Error: {err}");
+    }
+}
+
+fn run() -> Result<()> {
+    let stop_rx = new_cancel_token().context("failed to init shutdown handler")?;
+    let h = pwloop::start_pw_thread(stop_rx).context("failed to start pipewire listener")?;
     let mut state = State::default();
+
+    let span = info_span!("msg_listener");
+    let _h = span.enter();
 
     for msg in h {
         match msg {
-            ActionType::EntryAdd(oid, e) => {
-                println!("#{oid} PW:{:?} - {}", e.kind, e.get_label());
-                state.devices.insert(oid, e);
+            ActionType::EntryAdd(oid, entry) => {
+                info!(oid, ?entry, "EntryAdd");
+                state.devices.insert(oid, entry);
             }
-            ActionType::VolumeChange(oid, vol) => {
-                match state.devices.get(&oid) {
-                    Some(e) => {
-                        // TODO
-                        println!(
-                            "#{oid} PW:{:?} {} - {}",
-                            e.kind,
-                            e.get_label(),
-                            format_volume(&vol)
-                        );
-                    }
-                    None => {
-                        println!("Warn: got VolumeChange event for orphan device/node {oid}");
-                    }
+            ActionType::VolumeChange(oid, vol) => match state.devices.get(&oid) {
+                Some(e) => {
+                    info!(oid, entry_name = e.get_label(), ?vol, "VolumeChange");
                 }
-            }
+                None => {
+                    warn!(oid, "got VolumeChange event for orphan device/node");
+                }
+            },
             ActionType::EntryRemove(oid) => {
                 match state.devices.get(&oid) {
-                    Some(e) => {
+                    Some(entry) => {
                         // TODO
-                        println!("#{oid} PW:{:?} {} - Disconnected", e.kind, e.get_label(),);
+                        info!(oid, ?entry, "EntryRemove");
+                        continue;
                     }
                     None => {
-                        println!("Warn: got EntryRemove event for orphan device/node {oid}");
+                        warn!(oid, "got VolumeChange event for orphan device/node");
                     }
                 }
             }
             ActionType::Shutdown => {
-                println!("bye!");
-                return;
+                info!("bye!");
+                break;
             }
         }
     }
+
+    Ok(())
 }
--- src/pwloop.rs
+++ src/pwloop.rs
@@ -2,8 +2,14 @@ use crate::{state::ActionType, utils};
 use anyhow::{Context, Result};
 use pipewire::{self as pw, proxy::ProxyT, spa::param::ParamType};
 use pw::types::ObjectType;
+use tracing::{debug, debug_span, error, info};
 use utils::{PWContext, PWContextRc, PWGlobalObject};
 
+#[tracing::instrument(
+    name = "subscribe_device",
+    skip(ctx, sender, dev),
+    fields(device_id = dev.upcast_ref().id()),
+)]
 fn subscribe_device(ctx: PWContextRc, sender: ActionSender, dev: pw::device::Device) -> Result<()> {
     dev.subscribe_params(&[
         pw::spa::param::ParamType::Props,
@@ -12,29 +18,41 @@ fn subscribe_device(ctx: PWContextRc, sender: ActionSender, dev: pw::device::Dev
 
     let rm_sender = sender.clone();
     ctx.removed_listener(
-        ctx.device_listener_local(dev, move |oid, b| {
+        ctx.device_listener_local(dev, move |dev_id, b| {
             let vol_sender = sender.clone();
             b.param(move |_seq, param_type, _idx, _next, param| {
+                let span = debug_span!("device_listener", dev_id);
+                let _g = span.enter();
+
                 if param_type != ParamType::Props {
                     return;
                 }
 
                 // TODO: support other prop change events?
                 if let Some(vol) = param.and_then(utils::volume_from_pod) {
-                    // println!("pw: volume event: Object=Device; Id={oid} Vol={vol:?};");
-                    let _ = vol_sender.send(ActionType::VolumeChange(oid, vol));
+                    debug!(%dev_id, volume = ?vol, "device volume change");
+                    let _ = vol_sender.send(ActionType::VolumeChange(dev_id, vol));
                 }
             })
         }),
         Box::new(move |oid: u32| {
+            let span = tracing::debug_span!("device_removed", node_id = oid,);
+            let _g = span.enter();
+
+            debug!(%oid, "device removed");
             if let Err(err) = rm_sender.send(ActionType::EntryRemove(oid)) {
-                eprintln!("pw: failed to dispatch EntryRemove({oid}): {err}",);
+                error!(%oid, ?err, "failed to dispatch EntryRemove");
             }
         }),
     )?;
     Ok(())
 }
 
+#[tracing::instrument(
+    name = "subscribe_node",
+    skip(ctx, sender, node),
+    fields(node_id = node.upcast_ref().id()),
+)]
 fn subscribe_node(ctx: PWContextRc, sender: ActionSender, node: pw::node::Node) -> Result<()> {
     node.subscribe_params(&[
         pw::spa::param::ParamType::Props,
@@ -43,29 +61,41 @@ fn subscribe_node(ctx: PWContextRc, sender: ActionSender, node: pw::node::Node) 
 
     let rm_sender = sender.clone();
     ctx.removed_listener(
-        ctx.node_listener_local(node, move |oid, b| {
+        ctx.node_listener_local(node, move |node_id, b| {
             let vol_sender = sender.clone();
             b.param(move |_seq, param_type, _idx, _next, param| {
+                let span = debug_span!("node_listener", node_id);
+                let _g = span.enter();
+
                 if param_type != ParamType::Props {
                     return;
                 }
 
                 // TODO: support other prop change events?
                 if let Some(vol) = param.and_then(utils::volume_from_pod) {
-                    // println!("pw: volume event: Object=Node; Id={oid} Vol={vol:?};");
-                    let _ = vol_sender.send(ActionType::VolumeChange(oid, vol));
+                    debug!(%node_id, volume = ?vol, "node volume change");
+                    let _ = vol_sender.send(ActionType::VolumeChange(node_id, vol));
                 }
             })
         }),
         Box::new(move |oid: u32| {
+            let span = tracing::debug_span!("node_removed", node_id = oid,);
+            let _g = span.enter();
+
+            debug!(%oid, "node removed");
             if let Err(err) = rm_sender.send(ActionType::EntryRemove(oid)) {
-                eprintln!("pw: failed to dispatch EntryRemove({oid}): {err}",);
+                error!(%oid, ?err, "failed to dispatch EntryRemove");
             }
         }),
     )?;
     Ok(())
 }
 
+#[tracing::instrument(
+    name = "global_change",
+    skip(ctx, sender, o),
+    fields(obj_id = o.id),
+)]
 fn on_global_change(ctx: PWContextRc, sender: ActionSender, o: &PWGlobalObject) -> Result<()> {
     let entry = match utils::parse_object(o) {
         Some(e) => e,
@@ -74,19 +104,21 @@ fn on_global_change(ctx: PWContextRc, sender: ActionSender, o: &PWGlobalObject) 
         }
     };
 
-    println!("pw: subscribe to {}", entry.get_label());
-
+    let label = utils::format_object_label(o);
     match o.type_ {
         ObjectType::Node if utils::is_audio_node(&o.props) => {
-            let node: pw::node::Node = ctx.registry.bind(o).with_context(|| {
-                format!("failed to bind node #{}", utils::format_object_label(o))
-            })?;
+            let node: pw::node::Node = ctx
+                .registry
+                .bind(o)
+                .with_context(|| format!("failed to bind node #{}", &label))?;
 
-            let oid = node.upcast_ref().id();
-            if let Err(err) = sender.send(ActionType::EntryAdd(oid, entry)) {
-                eprintln!(
-                    "pw: failed to dispatch EntryAdd({:?} {}): {err}",
-                    o.type_, o.id,
+            let node_id = node.upcast_ref().id();
+            debug!(node_id, label = &label, "new node");
+            if let Err(err) = sender.send(ActionType::EntryAdd(node_id, entry)) {
+                error!(
+                    node_id,
+                    label = &label,
+                    "failed to dispatch EntryAdd: {err}"
                 );
             }
 
@@ -97,12 +129,10 @@ fn on_global_change(ctx: PWContextRc, sender: ActionSender, o: &PWGlobalObject) 
                 format!("failed to bind device {}", utils::format_object_label(o))
             })?;
 
-            let oid = dev.upcast_ref().id();
-            if let Err(err) = sender.send(ActionType::EntryAdd(oid, entry)) {
-                eprintln!(
-                    "pw: failed to dispatch EntryAdd({:?} {}): {err}",
-                    o.type_, o.id,
-                );
+            let dev_id = dev.upcast_ref().id();
+            debug!(dev_id, label = &label, "new device");
+            if let Err(err) = sender.send(ActionType::EntryAdd(dev_id, entry)) {
+                error!(dev_id, label = &label, "failed to dispatch EntryAdd: {err}");
             }
 
             subscribe_device(ctx, sender, dev)?;
@@ -120,14 +150,17 @@ pub fn start_pw_thread(cancel_token: std::sync::mpsc::Receiver<()>) -> Result<Ac
     let (tx, rx) = std::sync::mpsc::sync_channel::<ActionType>(3);
 
     let _h = std::thread::spawn(move || {
-        println!("pw: initializing...");
+        let span = tracing::info_span!("pw");
+        let _h = span.enter();
+
+        debug!("initializing pipewire");
         pw::init();
-        println!("pw: initialized");
+        debug!("initialized");
 
         let pwctx = match PWContext::new_shared() {
             Ok(r) => r,
             Err(err) => {
-                eprintln!("failed to build pipewire consumer: {err}");
+                error!("failed to build pipewire consumer: {err}");
                 return;
             }
         };
@@ -136,23 +169,23 @@ pub fn start_pw_thread(cancel_token: std::sync::mpsc::Receiver<()>) -> Result<Ac
         let cctx = pwctx.clone();
         let sent_tx = tx.clone();
 
-        println!("pw: registering listener...");
+        debug!("registering listener...");
         let _listener = pwctx
             .registry
             .add_listener_local()
             .global(move |global| {
                 if let Err(err) = on_global_change(cctx.clone(), sent_tx.clone(), global) {
-                    println!("pw:Error - {err}");
+                    error!("on global change hook returned an error: {err}");
                 }
             })
             .register();
 
-        println!("pw: starting thread loop...");
+        debug!("starting thread loop...");
         pwctx.begin(|| {
             // Suspend thread until cancellation signal is sent.
             // PW's ThreadLoop already manages its own thread under the hood.
             cancel_token.recv().ok();
-            println!("pw: shutting down...");
+            info!("shutting down...");
         });
 
         let _ = tx.send(ActionType::Shutdown);
